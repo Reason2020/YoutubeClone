318f769c91e3ad4ceb16bf8601f2ac192753ca19
Reason2020 <sthareason264@gmail.com>
2024-01-09 21:39:04 +0545

    Tags container implemented and slider functionality implemented

--- client/src/components/tagsContainer/TagsContainer.jsx
+++ client/src/components/tagsContainer/TagsContainer.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import TagCard from '../tagCard/TagCard';
 import './TagsContainer.scss';
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 
 const TagsContainer = () => {
-    const dummyTags = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const [ distance, setDistance ] = useState(0);
+  const tagsListRef = useRef(null); 
+
+  const handleArrowClick= (direction) => {
+    const tagsListContainerWidth = tagsListRef.current.clientWidth - 1000;
+    console.log("Width: ", tagsListContainerWidth);
+    console.log("Distance: ", distance);
+    if (direction === "left" && distance !== 0) {
+      let newDistance = distance;
+      setDistance(prevDistance => prevDistance + 100);
+      newDistance += 100;
+      console.log(newDistance);
+      tagsListRef.current.style.transform = `translateX(${newDistance}px)`
+    } else if (direction === "right" && distance > -tagsListContainerWidth) {
+      let newDistance = distance;
+      setDistance(prevDistance => prevDistance - 100);
+      newDistance -= 100;
+      tagsListRef.current.style.transform = `translateX(${newDistance}px)`;
+    }
+  }
+
+  const dummyTags = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   return (
-    <div className='tags_container'>
-        {
-            dummyTags.map((item, index) => (
-                <TagCard />
-            ))
-        }
+    <div className="tags_list">
+      <div className="tags_wrapper">
+        <button 
+          className='arrow left'
+          onClick={() => handleArrowClick("left")}>
+          <ArrowBackIos 
+            fontSize='small' />
+        </button>
+        <div className='tags_container' ref={tagsListRef}>
+          {
+              dummyTags.map((item, index) => (
+                  <TagCard />
+              ))
+          }
+        </div>
+        <button 
+          className='arrow right'
+          onClick={() => handleArrowClick("right")}>
+          <ArrowForwardIos 
+            fontSize='small'/>
+        </button>
+      </div>
     </div>
   )
 }
